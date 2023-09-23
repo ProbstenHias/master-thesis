a9061a00dbdc2ee9f707f5231fab6acc3b894621
Matthias Weilinger <matthias.weilinger@gmx.de>
2023-09-23 08:05:20 +0200

    update ui implementation

--- code/ui_schema_coordinator.py
+++ code/ui_schema_coordinator.py
@@ -1,35 +1,7 @@
-from qhana_plugin_runner.api.util import FileUrl, FrontendFormBaseSchema, PluginUrl
+from qhana_plugin_runner.api.util import FrontendFormBaseSchema, PluginUrl
+
 
 class OptimizerSetupTaskInputSchema(FrontendFormBaseSchema):
-    input_file_url = FileUrl(
-        required=True,
-        allow_none=False,
-        data_input_type="*",
-        data_content_types=["text/csv"],
-        metadata={
-            "label": "Dataset URL",
-            "description": "URL to a csv file with optimizable data.",
-        },
-    )
-    target_variable = marshmallow.fields.String(
-        required=True,
-        allow_none=False,
-        metadata={
-            "label": "Target Variable",
-            "description": "Name of the target variable in the dataset.",
-            "input_type": "text",
-        },
-    )
-    objective_function_plugin_selector = PluginUrl(
-        required=True,
-        allow_none=False,
-        plugin_tags=["objective-function"],
-        metadata={
-            "label": "Objective-Function Plugin Selector",
-            "description": "URL of objective-function-plugin.",
-            "input_type": "text",
-        },
-    )
     minimizer_plugin_selector = PluginUrl(
         required=True,
         allow_none=False,
@@ -40,3 +12,5 @@ class OptimizerSetupTaskInputSchema(FrontendFormBaseSchema):
             "input_type": "text",
         },
     )
+
+    # more fields...
